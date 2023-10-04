de0c22e436480dec2b6beb862b9548247439821f
Shawn0050 <shunlin0503@gmail.com>
2023-10-04 19:31:25 +0800

    update

--- 選擇權-評價與應用/options_strategy_report.py
+++ 選擇權-評價與應用/options_strategy_report.py
@@ -1,4 +1,5 @@
 # %%
+import pandas as pd
 import numpy as np
 import plotly.graph_objs as go
 import nbformat
@@ -100,43 +101,13 @@ def long_call_spread(call_premium,call_strike_price,stock_prices,strategy_name):
                     y=a,
                    mode='lines',
                    name='+C',
-                   line=dict(color='blue')
+                   line=dict(color='blue', dash='dash')
                    )
     trace2 = go.Scatter(x=prices_range,
                 y=b,
                 mode='lines',
                 name='-C',
-                line=dict(color='blue')
-                )
-    trace3 = go.Scatter(x=prices_range,
-                y=c,
-                mode='lines',
-                name='Strategy',
-                line=dict(color='blue')
-                )
-    data = [trace1, trace2, trace3]
-    draw_PnL_withpremium(data, strategy_name)
-    # 含手續費
-    fee = int(input('單邊手續費'))
-    stock_prices = stock_prices
-    strike_price = call_strike_price[0]
-    option_premium = call_premium[0]
-    a = BC_profits(stock_prices,strike_price,option_premium,fee)
-    strike_price = call_strike_price[1]
-    option_premium = call_premium[1]
-    b = SC_profits(stock_prices,strike_price,option_premium,fee)
-    c = a + b
-    trace1 = go.Scatter(x=prices_range,
-                y=a,
-                mode='lines',
-                name='+C',
-                line=dict(color='blue')
-                )
-    trace2 = go.Scatter(x=prices_range,
-                y=b,
-                mode='lines',
-                name='-C',
-                line=dict(color='red')
+                line=dict(color='red', dash='dash')
                 )
     trace3 = go.Scatter(x=prices_range,
                 y=c,
@@ -145,7 +116,42 @@ def long_call_spread(call_premium,call_strike_price,stock_prices,strategy_name):
                 line=dict(color='green')
                 )
     data = [trace1, trace2, trace3]
-    draw_PnL_withpremiumandfees(data, strategy_name)
+    draw_PnL_withpremium(data, strategy_name)
+    df = pd.DataFrame()
+    df['策略'] = ['+C','-C']
+    df['履約價格'] = [call_strike_price[0],call_strike_price[1]]
+    df['權利金點數'] = [call_premium[0],call_premium[1]]
+    print(df)
+    # 含手續費
+    # fee = int(input('單邊手續費'))
+    # stock_prices = stock_prices
+    # strike_price = call_strike_price[0]
+    # option_premium = call_premium[0]
+    # a = BC_profits(stock_prices,strike_price,option_premium,fee)
+    # strike_price = call_strike_price[1]
+    # option_premium = call_premium[1]
+    # b = SC_profits(stock_prices,strike_price,option_premium,fee)
+    # c = a + b
+    # trace1 = go.Scatter(x=prices_range,
+    #             y=a,
+    #             mode='lines',
+    #             name='+C',
+    #             line=dict(color='blue')
+    #             )
+    # trace2 = go.Scatter(x=prices_range,
+    #             y=b,
+    #             mode='lines',
+    #             name='-C',
+    #             line=dict(color='red')
+    #             )
+    # trace3 = go.Scatter(x=prices_range,
+    #             y=c,
+    #             mode='lines',
+    #             name='Strategy',
+    #             line=dict(color='green')
+    #             )
+    # data = [trace1, trace2, trace3]
+    # draw_PnL_withpremiumandfees(data, strategy_name)
 
 def long_put_spread(put_premium,put_strike_price,stock_prices,strategy_name):
     stock_prices = stock_prices
@@ -180,35 +186,36 @@ def long_put_spread(put_premium,put_strike_price,stock_prices,strategy_name):
     data = [trace1, trace2, trace3]
     draw_PnL_withpremium(data, strategy_name)
     # 含手續費
-    fee = int(input('單邊手續費'))
-    stock_prices = stock_prices
-    strike_price = put_strike_price[0]
-    option_premium = put_premium[0]
-    a = BP_profits(stock_prices,strike_price,option_premium,fee)
-    strike_price = put_strike_price[1]
-    option_premium = put_premium[1]
-    b = SP_profits(stock_prices,strike_price,option_premium,fee)
-    c = a + b
-    trace1 = go.Scatter(x=prices_range,
-                y=a,
-                mode='lines',
-                name='+P',
-                line=dict(color='blue')
-                )
-    trace2 = go.Scatter(x=prices_range,
-                y=b,
-                mode='lines',
-                name='-P',
-                line=dict(color='red')
-                )
-    trace3 = go.Scatter(x=prices_range,
-                y=c,
-                mode='lines',
-                name='Strategy',
-                line=dict(color='green')
-                )
-    data = [trace1, trace2, trace3]
-    draw_PnL_withpremiumandfees(data, strategy_name)
+    # fee = int(input('單邊手續費'))
+    # stock_prices = stock_prices
+    # strike_price = put_strike_price[0]
+    # option_premium = put_premium[0]
+    # a = BP_profits(stock_prices,strike_price,option_premium,fee)
+    # strike_price = put_strike_price[1]
+    # option_premium = put_premium[1]
+    # b = SP_profits(stock_prices,strike_price,option_premium,fee)
+    # c = a + b
+    # trace1 = go.Scatter(x=prices_range,
+    #             y=a,
+    #             mode='lines',
+    #             name='+P',
+    #             line=dict(color='blue')
+    #             )
+    # trace2 = go.Scatter(x=prices_range,
+    #             y=b,
+    #             mode='lines',
+    #             name='-P',
+    #             line=dict(color='red')
+    #             )
+    # trace3 = go.Scatter(x=prices_range,
+    #             y=c,
+    #             mode='lines',
+    #             name='Strategy',
+    #             line=dict(color='green')
+    #             )
+    # data = [trace1, trace2, trace3]
+    # draw_PnL_withpremiumandfees(data, strategy_name)
+
 def spread(call_premium,call_strike_price,put_premium,put_strike_price,stock_prices,strategy_name):
     stock_prices = stock_prices
     strike_price = put_strike_price[0]
@@ -235,25 +242,25 @@ def spread(call_premium,call_strike_price,put_premium,put_strike_price,stock_pri
                     y=a,
                    mode='lines',
                    name='+P',
-                   line=dict(color='blue')
+                   line=dict(color='blue', dash='dash')
                    )
     trace2 = go.Scatter(x=prices_range,
                 y=b,
                 mode='lines',
                 name='-P',
-                line=dict(color='red')
+                line=dict(color='red', dash='dash')
                 )
     trace3 = go.Scatter(x=prices_range,
                     y=c,
                    mode='lines',
                    name='+C',
-                   line=dict(color='lightblue')
+                   line=dict(color='lightblue', dash='dash')
                    )
     trace4 = go.Scatter(x=prices_range,
                 y=d,
                 mode='lines',
                 name='-C',
-                line=dict(color='pink')
+                line=dict(color='pink', dash='dash')
                 )
     trace5 = go.Scatter(x=prices_range,
                 y=e,
@@ -263,104 +270,109 @@ def spread(call_premium,call_strike_price,put_premium,put_strike_price,stock_pri
                 )
     data = [trace1, trace2, trace3, trace4, trace5]
     draw_PnL_withpremium(data, strategy_name)
+    df = pd.DataFrame()
+    df['策略'] = ['+P','-P','+C','-C']
+    df['履約價格'] = [put_strike_price[0],put_strike_price[1],call_strike_price[0],call_strike_price[1]]
+    df['權利金點數'] = [put_premium[0], put_premium[1], call_premium[0],call_premium[1]]
+    print(df)
     # 含手續費
-    fee = int(input('單邊手續費'))
-    stock_prices = stock_prices
-    strike_price = put_strike_price[0]
-    option_premium = put_premium[0] 
-    a = BP_profits(stock_prices,strike_price,option_premium,fee)
-    stock_prices = stock_prices
-    strike_price = put_strike_price[1]
-    option_premium = put_premium[1] 
-    b = SP_profits(stock_prices,strike_price,option_premium,fee)
-    stock_prices = stock_prices
-    strike_price = call_strike_price[0]
-    option_premium = call_premium[0] 
-    c = BC_profits(stock_prices,strike_price,option_premium,fee)
-    stock_prices = stock_prices
-    strike_price = call_strike_price[1]
-    option_premium = call_premium[1] 
-    d = SC_profits(stock_prices,strike_price,option_premium,fee)
-    e = a + b + c + d
-    trace1 = go.Scatter(x=prices_range,
-                    y=a,
-                   mode='lines',
-                   name='+P',
-                   line=dict(color='blue')
-                   )
-    trace2 = go.Scatter(x=prices_range,
-                y=b,
-                mode='lines',
-                name='-P',
-                line=dict(color='red')
-                )
-    trace3 = go.Scatter(x=prices_range,
-                    y=c,
-                   mode='lines',
-                   name='+C',
-                   line=dict(color='lightblue')
-                   )
-    trace4 = go.Scatter(x=prices_range,
-                y=d,
-                mode='lines',
-                name='-C',
-                line=dict(color='pink')
-                )
-    trace5 = go.Scatter(x=prices_range,
-                y=e,
-                mode='lines',
-                name='Strategy',
-                line=dict(color='green')
-                )
-    data = [trace1, trace2, trace3, trace4, trace5]
-    draw_PnL_withpremiumandfees(data, strategy_name)
+    # fee = int(input('單邊手續費'))
+    # stock_prices = stock_prices
+    # strike_price = put_strike_price[0]
+    # option_premium = put_premium[0] 
+    # a = BP_profits(stock_prices,strike_price,option_premium,fee)
+    # stock_prices = stock_prices
+    # strike_price = put_strike_price[1]
+    # option_premium = put_premium[1] 
+    # b = SP_profits(stock_prices,strike_price,option_premium,fee)
+    # stock_prices = stock_prices
+    # strike_price = call_strike_price[0]
+    # option_premium = call_premium[0] 
+    # c = BC_profits(stock_prices,strike_price,option_premium,fee)
+    # stock_prices = stock_prices
+    # strike_price = call_strike_price[1]
+    # option_premium = call_premium[1] 
+    # d = SC_profits(stock_prices,strike_price,option_premium,fee)
+    # e = a + b + c + d
+    # trace1 = go.Scatter(x=prices_range,
+    #                 y=a,
+    #                mode='lines',
+    #                name='+P',
+    #                line=dict(color='blue')
+    #                )
+    # trace2 = go.Scatter(x=prices_range,
+    #             y=b,
+    #             mode='lines',
+    #             name='-P',
+    #             line=dict(color='red')
+    #             )
+    # trace3 = go.Scatter(x=prices_range,
+    #                 y=c,
+    #                mode='lines',
+    #                name='+C',
+    #                line=dict(color='lightblue')
+    #                )
+    # trace4 = go.Scatter(x=prices_range,
+    #             y=d,
+    #             mode='lines',
+    #             name='-C',
+    #             line=dict(color='pink')
+    #             )
+    # trace5 = go.Scatter(x=prices_range,
+    #             y=e,
+    #             mode='lines',
+    #             name='Strategy',
+    #             line=dict(color='green')
+    #             )
+    # data = [trace1, trace2, trace3, trace4, trace5]
+    # draw_PnL_withpremiumandfees(data, strategy_name)
 
 strategy_name = input(str('策略名稱：（買權多頭價差or賣權多頭價差or投資組合）'))
 if strategy_name == '買權多頭價差':
     # premium
-    call1_premium = int(input('Call1權利金：'))
-    call2_premium = int(input('Call2權利金：'))
+    call1_premium = float(input('Call1權利金：'))
+    call2_premium = float(input('Call2權利金：'))
     # strike_price
-    call1_strike_price = int(input('Call1履約價：'))
-    call2_strike_price = int(input('Call2履約價：'))
+    call1_strike_price = float(input('Call1履約價：'))
+    call2_strike_price = float(input('Call2履約價：'))
     call_premium = [call1_premium, call2_premium]
     call_strike_price = [call1_strike_price, call2_strike_price]
     # 定義股票價格區間
-    prices_range = np.array(range(16000, 17000, 1))
+    prices_range = np.array(range(15500, 17000, 1))
     stock_prices = prices_range
     long_call_spread(call_premium,call_strike_price,stock_prices,strategy_name)
 if strategy_name == '賣權多頭價差': 
     # premium
-    put1_premium = int(input('Put1權利金：'))
-    put2_premium = int(input('Put2權利金：'))
+    put1_premium = float(input('Put1權利金：'))
+    put2_premium = float(input('Put2權利金：'))
     # strike_price
-    put1_strike_price = int(input('Put1履約價：'))
-    put2_strike_price = int(input('Put2履約價：'))
+    put1_strike_price = float(input('Put1履約價：'))
+    put2_strike_price = float(input('Put2履約價：'))
     put_premium = [put1_premium, put2_premium]
     put_strike_price = [put1_strike_price, put2_strike_price]
     # 定義股票價格區間
-    prices_range = np.array(range(16000, 17000, 1))
+    prices_range = np.array(range(15500, 17000, 1))
     stock_prices = prices_range
     long_put_spread(put_premium,put_strike_price,stock_prices,strategy_name)
 if strategy_name == '投資組合':
     # premium
-    call1_premium = int(input('Call1權利金：'))
-    call2_premium = int(input('Call2權利金：'))
+    call1_premium = float(input('Call1權利金：'))
+    call2_premium = float(input('Call2權利金：'))
     # strike_price
-    call1_strike_price = int(input('Call1履約價：'))
-    call2_strike_price = int(input('Call2履約價：'))
+    call1_strike_price = float(input('Call1履約價：'))
+    call2_strike_price = float(input('Call2履約價：'))
     call_premium = [call1_premium, call2_premium]
     call_strike_price = [call1_strike_price, call2_strike_price]
     # premium
-    put1_premium = int(input('Put1權利金：'))
-    put2_premium = int(input('Put2權利金：'))
+    put1_premium = float(input('Put1權利金：'))
+    put2_premium = float(input('Put2權利金：'))
     # strike_price
-    put1_strike_price = int(input('Put1履約價：'))
-    put2_strike_price = int(input('Put2履約價：'))
+    put1_strike_price = float(input('Put1履約價：'))
+    put2_strike_price = float(input('Put2履約價：'))
     put_premium = [put1_premium, put2_premium]
     put_strike_price = [put1_strike_price, put2_strike_price]
     # 定義股票價格區間
-    prices_range = np.array(range(16000, 17000, 1))
+    prices_range = np.array(range(15500, 17000, 1))
     stock_prices = prices_range
     spread(call_premium,call_strike_price,put_premium,put_strike_price,stock_prices,strategy_name)
 # %%
