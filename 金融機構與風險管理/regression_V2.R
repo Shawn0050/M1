4afb9421d6986cb859075a2ee456fec4bef568cd
Shawn0050 <shunlin0503@gmail.com>
2023-11-13 20:37:16 +0800

    update

--- 金融機構與風險管理/regression_V2.R
+++ 金融機構與風險管理/regression_V2.R
@@ -17,7 +17,7 @@ model_ols <- lm(data$rating_diff ~ data$CAPEI_change + data$bm_change + data$evm
 summary(model_ols)
 stargazer(model_ols, type = "latex")
 
-# Logit
+# Ordered Logit
 data$rating_diff <- factor(data$rating_diff, ordered = TRUE)
 model_logit <- polr(data$rating_diff ~ data$CAPEI_change + data$bm_change + data$evm_change + data$pe_op_basic_change + 
                   data$pe_op_dil_change + data$pe_exi_change + data$pe_inc_change + data$ps_change + data$pcf_change + 
@@ -26,7 +26,7 @@ model_logit <- polr(data$rating_diff ~ data$CAPEI_change + data$bm_change + data
                   data$aftret_equity_change + data$cash_lt_change, data = data, Hess = TRUE)
 summary(model_logit)
 
-# Probit
+# Ordered Probit
 model_probit <- polr(data$rating_diff ~ data$CAPEI_change + data$bm_change + data$evm_change + data$pe_op_basic_change + 
                       data$pe_op_dil_change + data$pe_exi_change + data$pe_inc_change + data$ps_change + data$pcf_change + 
                       data$dpr_change + data$npm_change + data$opmbd_change + data$gpm_change + data$ptpm_change + data$roa_change +
